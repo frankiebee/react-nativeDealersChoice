48335b95b14f81078713fda7bf01d446c7e20e20
Frankie <frankie.diamond@gmail.com>
2016-04-25 10:38:35 -0700

    Clean up api module

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -4,16 +4,7 @@ var api = {
       method: "GET",
     }).then((response) => response.json());
   },
-  getBio(username){
-    var username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}`;
-    return fetch(url).then((res) => res.json())
-  },
-  getRepos(username){
-    var username = username.toLowerCase().trim();
-    var url = `https://api.github.com/users/${username}/repos`;
-    return fetch(url).then((res) => res.json());
-  },
+
   loginPage(email, password){
      email = email.toLowerCase().trim();
      return fetch("http://localhost:3000/auth/sign_in?email=" + email + "&password=" + password, {
